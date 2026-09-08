def98c6424e39eb1eacd81830adfe184b2d496a5
acarigna <amcarigna@gmail.com>
2026-09-08 12:55:16 -0400

    add requirements, start main

--- main.py
+++ main.py
@@ -1,16 +1,11 @@
-# This is a sample Python script.
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+jfile = open('default-cards-20260907090537.jsonl', 'r')
+count = 0
+for line in jfile:
+    card = json.loads(line)
+    if card['name'] == "Cultivate":
+        print(card)
+        count += 1
+print(count)
+jfile.close()
